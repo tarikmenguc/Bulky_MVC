e03f20e2e36817bd2c2eaf2629ad2a04184eb05f
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2024-10-29 12:32:50 +0300

    Kategori listeleme işlevi eklendi

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public int DisplayOrder { get; set; }
+        [Required]
+        public  string Name { get; set; }
+       
+    }
+}
