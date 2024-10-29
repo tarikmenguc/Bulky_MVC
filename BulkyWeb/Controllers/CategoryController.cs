e6c1d301d8bb07e5418aae62c4e5aee563a5686c
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2024-10-29 17:53:05 +0300

    edit kısmı için görünüm ve birkaç ekleme

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -23,12 +23,46 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "the Display Order cannot exactly match the name ");
+            }
+
             if (ModelState.IsValid) {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
            
+            return View();
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id== null || id== 0)
+            {
+                return NotFound();
+            }
+            Category categoryfromdb = _db.Categories.Find(id);
+            if (categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+           
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
     }
