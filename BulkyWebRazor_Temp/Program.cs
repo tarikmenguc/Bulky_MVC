edde86f98c240b8b8fdf70d98fdd62f4568bbcf6
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2024-10-31 12:34:31 +0300

    Added Ef core in Razor project

--- BulkyWebRazor_Temp/Program.cs
+++ BulkyWebRazor_Temp/Program.cs
@@ -1,7 +1,40 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddRazorPages();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+//app.Run();
+
+using BulkyWebRazor_Temp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add ApplicationDbContext to the services
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -23,3 +56,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
