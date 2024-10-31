46b82e7ad95239914ea531517adb38e93b2e4b85
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2024-10-31 11:53:47 +0300

    added Razor page project

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be 1-100")]
+        public int DisplayOrder { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage = "Name lenght must be 1-30")]
+        public string Name { get; set; }
+
+    
+}
+}
