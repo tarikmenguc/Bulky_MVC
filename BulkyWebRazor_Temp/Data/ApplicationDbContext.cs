8524ccbd6d1956b6aa85b2f4235c3228a63654bd
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2024-11-01 14:23:42 +0300

    Added categories in razor project

--- BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace BulkyWebRazor_Temp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
@@ -14,7 +14,7 @@ namespace BulkyWebRazor_Temp.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+               new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { ID = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { ID = 3, Name = "History", DisplayOrder = 3 }
                 );
